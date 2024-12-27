94d14cb2003a1dd391f724bec27bb3d587907ff4
Zining919 <126067910+Zining919@users.noreply.github.com>
2024-12-27 10:58:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cj1rxhiu5cwo.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "wongzining-employee"
 customregion = "us-east-1"
-
